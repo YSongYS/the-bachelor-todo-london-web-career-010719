b6db97fb0213d2627f38c8706b3aee38db056ae1
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 14:17:45 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -32,11 +32,10 @@ def get_occupation(data, hometown)
   data.each do |season_name,season_people|
     season_people.each do |contestant|
       if contestant.has_value?(hometown)
-        return contestant[:"occupation"]
+        puts contestant[:"occupation"]
       end
     end
   end
-  counter
 end
 
 def get_average_age_for_season(data, season)
