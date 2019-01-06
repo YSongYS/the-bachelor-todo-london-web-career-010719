822a6512caeb99572b506b70c2db36bddc88c18e
Songyishu Yang <songyishu.yang@gmail.com>
2019-01-06 13:42:19 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,6 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |suiter|
+    puts suiter
     return suiter[:name].split(" ")[0] if suiter.has_value?("winner")
   end
 end
